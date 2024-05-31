962997457f8f7d64d37affeb7d2248a567e60e4e
whitehwarang <32785873+whitehwarang@users.noreply.github.com>
2024-05-31 18:42:41 +0900

    Update Const.py
    증권사 명칭 및 도메인 변경 반영

--- Const.py
+++ Const.py
@@ -1,6 +1,9 @@
 
+# 24.06.01. EBEST 투자증권 --(명칭변경)--> LS투자증권
+# BASE_URL_POST = "https://openapi.ebestsec.co.kr:8080"
+# BASE_URL_WEBSOCKET = "wss://openapi.ebestsec.co.kr:9443"
+# BASE_URL_WEBSOCKET_FOR_TEST = "wss://openapi.ebestsec.co.kr:29443"
 
-BASE_URL_POST = "https://openapi.ebestsec.co.kr:8080"
-
-BASE_URL_WEBSOCKET = "wss://openapi.ebestsec.co.kr:9443"
-BASE_URL_WEBSOCKET_FOR_TEST = "wss://openapi.ebestsec.co.kr:29443"
+BASE_URL_POST = "https://openapi.ls-sec.co.kr:8080"
+BASE_URL_WEBSOCKET = "wss://openapi.ls-sec.co.kr:9443"
+BASE_URL_WEBSOCKET_FOR_TEST = "wss://openapi.ls-sec.co.kr:29443"
