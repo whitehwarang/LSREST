41ba659c34f7e78f76821cfb412ff9a82657a2c5
whitehwarang <32785873+whitehwarang@users.noreply.github.com>
2025-04-15 04:20:27 +0900

    Update Util.py
    AsyncSessionAccessTokenManager class 추가
    및 단순 오류 수정

--- Util.py
+++ Util.py
@@ -13,12 +13,20 @@ from . import Const
 def get_now_as_timestamp():  return datetime.datetime.today().timestamp()
 
 
+class Token(str): pass
+
+
 class AccessTokenManager:
     """
     AccessToken 관리하는 Context Manager Class.
     사용 예시 - 주식 차트 데이터 요청
-        # 아래와 같이 with절로 Access Token에 대한 자원관리 가능
-        with api.Util.AccessTokenManager(appkey=appkey, appsecretkey=appsecretkey, session=None, if_save=False) as access_token:
+        # with절로 Access Token 자원관리 가능
+        import LSREST as api
+        with api.Util.AccessTokenManager(
+            appkey=appkey, appsecretkey=appsecretkey, 
+            session=None, 
+            if_save=False
+            ) as access_token:
             tr_inst = api.t8410(token=my_token, shcode='005930', qrycnt=100, sdate='20230901', edate='20230926')
             daily_chart = api.Sync.rq_tr(tr_inst)
             print(daily_chart)
@@ -31,39 +39,38 @@ class AccessTokenManager:
         self.__if_save = if_save
         self.token_info = {}
     #
-    def __enter__(self) -> str:
+    def __enter__(self) -> Token:
         """ AccessTokenManager 진입 (동기) """
         # 최근 TOKEN 정보를 저장한 파일을 읽기
         self.token_info = self._read_token_info() if self.__if_save else {}
         # 기존 토큰정보에 이상이 있거나, 만료된 경우 새로 토큰 정보 요청
-        if self.__need_to_reissue():
+        if self.is_token_expired:
             self.__sync_issue_token()
-        return self.token_info['access_token']
+        return Token(self.token_info['access_token'])
     #
-    async def __aenter__(self) -> str:
+    async def __aenter__(self) -> Token:
         """ AccessTokenManager 진입 (비동기) """
         # 최근 TOKEN 정보를 저장한 파일을 읽기
         self.token_info = self._read_token_info() if self.__if_save else {}
-        if self.__need_to_reissue():
+        if self.is_token_expired:
             await self.__async_issue_token()
-        return self.token_info['access_token']
+        return Token(self.token_info['access_token'])
     #
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, exc_type, exc_value, exc_traceback) -> bool:
         """ AccessTokenManager 닫기 (동기) """  
         self.__sync_revoke_token()
         if exc_type:
-            print(exc_type, exc_value, traceback)
+            print(exc_type, exc_value, exc_traceback)
+            import traceback
+            traceback.print_tb(exc_traceback)
     #
-    async def __aexit__(self, exc_type, exc_value, traceback):
+    async def __aexit__(self, exc_type, exc_value, exc_traceback) -> bool:
         """ AccessTokenManager 닫기 (비동기) """
         await self.__async_revoke_token()
         if exc_type:
-            print(exc_type, exc_value, traceback)
-    #
-    def __need_to_reissue(self) -> bool:
-        """ 토큰 새발행이 필요한지 여부 확인 """
-        return  self.token_info.get("access_token") is None or \
-                self.is_token_expired
+            print(exc_type, exc_value, exc_traceback)
+            import traceback
+            traceback.print_tb(exc_traceback)
     #
     def __reset_token_info(self) -> None:
         """ self.token_info을 리셋하고 저장된 파일을 삭제"""
@@ -109,9 +116,10 @@ class AccessTokenManager:
     @property
     def is_token_expired(self) -> bool:
         """ token 만료 여부 확인 """
-        if  self.token_info.get('timestamp') is None or \
+        if  self.token_info.get("access_token") is None or \
+            self.token_info.get('timestamp') is None or \
             self.token_info.get('expires_in') is None:
-            return False
+            return True
         else:
             now = get_now_as_timestamp()
             return now >= self.token_info.get("timestamp") + self.token_info.get('expires_in')
@@ -134,4 +142,43 @@ class AccessTokenManager:
         with open(filepath, encoding='utf8', mode='w') as f:
             json.dump(self.token_info, f)
 
+
+class AsyncSessionAccessTokenManager(AccessTokenManager, aiohttp.ClientSession):
+    """
+    ClientSession과 함께 AccessToken을 관리하는 Context Manager Class.
+    사용 예시 - 주식 차트 데이터 요청
+        # async with절로 ClientSession과 AccessToken 동시 자원관리 가능
+        import LSREST as api
+        async def main():
+            async with api.Util.SessionAccessTokenManager(
+                appkey=appkey, appsecretkey=appsecretkey, if_save=False
+                ) as (session, access_token):
+                tr_inst = api.t8410(token=my_token, shcode='005930', qrycnt=100, sdate='20230901', edate='20230926')
+                daily_chart = await api.Async.rq_tr(session=session, tr_inst=tr_inst)
+                print(daily_chart)
+            # with 절 종료 시, session 자동 종료 및 access_token 자동 폐기(RevokeToken) 수행
+        asyncio.run(main())
+    """
+    def __init__(self, appkey, appsecretkey, if_save=False, **kwargs):
+        super(AccessTokenManager, self).__init__(base_url=Const.BASE_URL_POST, **kwargs)
+        super().__init__(appkey=appkey, appsecretkey=appsecretkey, session=self, if_save=if_save)        
+    #
+    def __enter__(self) -> Token:
+        """ 동기식으로는 사용할 수 없으므로, raise NotImplemented """
+        raise NotImplemented
+    #
+    async def __aenter__(self) -> tuple[aiohttp.ClientSession, Token]:
+        """ SessionAccessTokenManager 진입 (비동기) """
+        session: aiohttp.ClientSession = await super(AccessTokenManager, self).__aenter__()
+        token  : Token                 = await super().__aenter__()
+        return session, token
+    #
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        """ 동기식으로는 사용할 수 없으므로, raise NotImplemented """
+        raise NotImplemented
+    #
+    async def __aexit__(self, exc_type, exc_value, exc_traceback):
+        """ SessionAccessTokenManager 닫기 (비동기) """
+        await super().__aexit__(exc_type, exc_value, exc_traceback)
+        await super(AccessTokenManager, self).__aexit__(exc_type, exc_value, exc_traceback)
 
