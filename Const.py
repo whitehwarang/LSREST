38e793a336f7e677513e2ff01c52d0c8a861f1f2
whitehwarang <32785873+whitehwarang@users.noreply.github.com>
2025-04-14 14:55:49 +0900

    access_token 관련
    access_token 임시저장경로 추가
    (Util.AccessTokenManager(if_save=True)인 경우에만 해당 경로에 임시저장 후 Context 종료 시 자동 폐기 및 파일도 자동삭제)

--- Const.py
+++ Const.py
@@ -10,3 +10,8 @@ LS - OPEN API 고정값을 저장하는 파일
 BASE_URL_POST = "https://openapi.ls-sec.co.kr:8080"
 BASE_URL_WEBSOCKET = "wss://openapi.ls-sec.co.kr:9443"
 BASE_URL_WEBSOCKET_FOR_TEST = "wss://openapi.ls-sec.co.kr:29443"
+
+# ACCESS_TOKEN을 파일에 임시 저장
+RECENT_ACCESS_TOKEN_FILEPATH = "_recent_access_token.json"
+
+
